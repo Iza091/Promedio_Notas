2a243224da01d8bf51c4cda5cfdd29b3877bb41f
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2022-11-03 20:26:03 -0600

    Notas

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Promedio_Notas
-Calcular el promedio de las notas 
+Calcular el promedio de las notas utilizando a python como motor base
+y desarrollado por Pseint, vuela alto.
+
+Dev's: @Fernando1809 y @Iza091
---
+++ prom.py
@@ -0,0 +1,42 @@
+#COMPUTO 1
+print("COMPUTO 1")
+L1C1 = float(input("Ingrese L1 : "))
+n1= L1C1*0.30
+L2C1 = float(input("Ingrese L2 : "))
+n2= L2C1*0.30
+PC1= float(input("Ingrese P1 : "))
+n3=PC1*0.40
+
+prom1 = (n1+n2+n3)
+print("EL PROMEDIO COMPUTO 1 ES : ", prom1)
+
+
+#COMPUTO 2
+print("COMPUTO 2")
+L1C2 = float(input("Ingrese L1 : "))
+n1= L1C2*0.30
+L2C2 = float(input("Ingrese L2 : "))
+n2= L2C2*0.30
+PC2= float(input("Ingrese P1 : "))
+n3=PC2*0.40
+
+prom2 = (n1+n2+n3)
+print("EL PROMEDIO COMPUTO 2 ES : ", prom2)
+
+
+#COMPUTO 3
+print("COMPUTO 3")
+L1C3 = float(input("Ingrese L1 : "))
+n1= L1C3*0.30
+L2C3 = float(input("Ingrese L2 : "))
+n2= L2C3*0.30
+PC3= float(input("Ingrese P1 : "))
+n3=PC3*0.40
+
+prom3 = (n1+n2+n3)
+print("EL PROMEDIO COMPUTO 3 ES : ", prom3)
+
+
+print("TOTAL")
+promF=(prom1+prom2+prom3)/3
+print("Nota final es: ",promF)
